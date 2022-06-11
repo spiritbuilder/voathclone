902dee831d4cc9968b991c862650fe51ed9b02a8
spiritbuilder <olaifatimothy@gmail.com>
2022-06-11 05:47:58 +0100

    create models

---
+++ server/models/users.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+let schema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname:String,
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: [validator.isEmail, "wrong data in the email field"],
+  },
+  password: {
+    type: String,
+    min: 8,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("user", schema);
